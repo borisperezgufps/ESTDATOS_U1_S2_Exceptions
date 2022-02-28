6aaa9a11ff3320025f0e45c2cebbb5314a251d58
borisperezgufps <99198267+borisperezgufps@users.noreply.github.com>
2022-02-28 16:57:21 -0500

    first commit

---
+++ src/u1/s2/exceptions/Demonstration1.java
@@ -0,0 +1,19 @@
+package u1.s2.exceptions;
+
+public class Demonstration1 {
+	public static void main(String[] args) {
+		
+		System.out.println("***Demonstration-1.Exploring Exceptions.***");
+		
+		int a = 100;
+		int b = 2;
+		int result;
+		b -= 2;// b becomes 0
+		
+		result = a / b;
+		
+		System.out.println("The result of a/b is :" + result);
+	}
+}
+
+
